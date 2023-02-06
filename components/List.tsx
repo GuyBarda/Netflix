5f5ecd74f2c1bb9ef41c4ab7dcf615be45920d91
Guy Barda <guy.barda.1234@gmail.com>
2023-02-06 13:17:01 +0200

    home Ui finished

---
+++ components/List.tsx
@@ -0,0 +1,55 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { useRef, useState } from 'react';
+import { Movie } from '../typings';
+import MoviePreview from './MoviePreview';
+
+interface Props {
+    title: string;
+    movies: Movie[];
+}
+
+const List = ({ title, movies }: Props) => {
+    const listRef = useRef<HTMLDivElement>(null);
+    const [isMove, setIsMove] = useState(false);
+
+    const handleClick = (direction: string) => {
+        setIsMove(true);
+        if (listRef.current) {
+            const { scrollLeft, clientWidth } = listRef.current;
+
+            const scrollTo =
+                direction === 'left'
+                    ? scrollLeft - clientWidth
+                    : scrollLeft + clientWidth;
+            listRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <div className=" space-y-0.5 md:space-y-2">
+            <h1 className="capitalize w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+                {title.replace(/([A-Z])/g, ' $1')}
+            </h1>
+            <div className="group relative md:-ml-2">
+                <ChevronLeftIcon
+                    className={`carousel-icon ${!isMove && 'hidden'}`}
+                    onClick={() => handleClick('left')}
+                />
+                <div
+                    ref={listRef}
+                    className="overflow-y-hidden flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+                >
+                    {movies.map((movie) => (
+                        <MoviePreview key={movie.id} movie={movie} />
+                    ))}
+                </div>
+                <ChevronRightIcon
+                    className="carousel-icon"
+                    onClick={() => handleClick('right')}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default List;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,66 @@
-import Head from 'next/head'
-import  Hero  from '../components/Hero'
-import  Header  from '../components/Header'
-import requests from '../utils/requests'
-import { Movie } from '../typings'
+import Head from 'next/head';
+import Hero from '../components/Hero';
+import Header from '../components/Header';
+import requests from '../utils/requests';
+import { Movie } from '../typings';
+import List from '../components/List';
 
 interface Props {
-    netflixOriginals: Movie[]
-    trendingNow: Movie[]
-    topRated: Movie[]
-    actionMovies: Movie[]
-    comedyMovies: Movie[]
-    horrorMovies: Movie[]
-    romanceMovies: Movie[]
-    documentaries: Movie[]
+    netflixOriginals: Movie[];
+    trendingNow: Movie[];
+    topRated: Movie[];
+    actionMovies: Movie[];
+    comedyMovies: Movie[];
+    horrorMovies: Movie[];
+    romanceMovies: Movie[];
+    documentaries: Movie[];
 }
 
-const Home = ({netflixOriginals,trendingNow, topRated, actionMovies,comedyMovies,horrorMovies,romanceMovies,documentaries}: Props) => {
-    
+const Home = (props: Props) => {
+    const genres = [
+        'netflixOriginals',
+        'trendingNow',
+        'topRated',
+        'actionMovies',
+        'comedyMovies',
+        'horrorMovies',
+        'romanceMovies',
+        'documentaries',
+    ];
     return (
-        <div>
+        <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
             <Head>
                 <title>Netflix</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Header/>
+            <Header />
 
-
-            <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
-                <Hero randomMovie={netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]}/>
-
+            <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+                <Hero
+                    randomMovie={
+                        props.netflixOriginals[
+                            Math.floor(
+                                Math.random() * props.netflixOriginals.length
+                            )
+                        ]
+                    }
+                />
+                <section className="md:space-y-7">
+                    {genres.map((genre) => (
+                        <List
+                            title={genre}
+                            key={genre}
+                            movies={props[genre as keyof typeof props]}
+                        />
+                    ))}
+                </section>
             </main>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
 
 export const getServerSideProps = async () => {
     const [
@@ -47,16 +72,16 @@ export const getServerSideProps = async () => {
         horrorMovies,
         romanceMovies,
         documentaries,
-      ] = await Promise.all([
-            fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-            fetch(requests.fetchTrending).then((res) => res.json()),
-            fetch(requests.fetchTopRated).then((res) => res.json()),
-            fetch(requests.fetchActionMovies).then((res) => res.json()),
-            fetch(requests.fetchComedyMovies).then((res) => res.json()),
-            fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-            fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-            fetch(requests.fetchDocumentaries).then((res) => res.json()),
-      ])
+    ] = await Promise.all([
+        fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+        fetch(requests.fetchTrending).then((res) => res.json()),
+        fetch(requests.fetchTopRated).then((res) => res.json()),
+        fetch(requests.fetchActionMovies).then((res) => res.json()),
+        fetch(requests.fetchComedyMovies).then((res) => res.json()),
+        fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+        fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+        fetch(requests.fetchDocumentaries).then((res) => res.json()),
+    ]);
     return {
         props: {
             netflixOriginals: netflixOriginals.results,
@@ -68,5 +93,5 @@ export const getServerSideProps = async () => {
             romanceMovies: romanceMovies.results,
             documentaries: documentaries.results,
         },
-    }
-}
+    };
+};
